aeb35e6f324c4dd58aef56c336cd3a5de50aa355
Kaden Klein <kadenklein@MacBook-Pro.local>
2020-08-07 04:35:47 -0800

    fixed small styling

--- src/Components/Display/Projects/GroupMessages.js
+++ src/Components/Display/Projects/GroupMessages.js
@@ -7,7 +7,7 @@ import io from 'socket.io-client';
 let socket;
 
 const GroupMessages = (props) => {
-    const  ENDPOINT = 'https://167.172.193.114:3030';
+    const  ENDPOINT = 'https://konnecdevice.com';
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
 
