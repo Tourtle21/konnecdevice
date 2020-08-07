33427d7f75e00fb9221ba4c38dccfe3f4354d9aa
Kaden Klein <kadenklein@MacBook-Pro.local>
2020-08-07 02:57:00 -0800

    Deleted all console.log

--- src/ducks/routeReducer.js
+++ src/ducks/routeReducer.js
@@ -18,7 +18,6 @@ export default function reducer(state=initialState, action) {
     const {payload, type} = action;
     switch (type) {
         case GET_BOOL:
-            console.log(payload)
             return {...state, myIdeas: payload};
         default:
             return state;
