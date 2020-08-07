6977715674c1ca917cba2b5c49af0c8948efcaa6
Kaden Klein <kadenklein@MacBook-Pro.local>
2020-08-07 01:06:59 -0800

    Oh you have to build each tim

--- src/Components/Display/Projects/GroupMessages.js
+++ src/Components/Display/Projects/GroupMessages.js
@@ -7,7 +7,7 @@ import io from 'socket.io-client';
 let socket;
 
 const GroupMessages = (props) => {
-    const  ENDPOINT = 'http://167.172.193.114';
+    const  ENDPOINT = 'http://167.172.193.114:3030';
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
 
